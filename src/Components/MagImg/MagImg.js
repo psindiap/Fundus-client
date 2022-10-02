8d3262b4b6a82c14dd4bd9b54eb6d72e21f0159d
PSP <parthasarathipurkayastha001@gmail.com>
2022-10-02 20:54:39 +0530

    first commit

--- src/Components/MagImg/MagImg.js
+++ src/Components/MagImg/MagImg.js
@@ -17,7 +17,7 @@ function importAll(r) {
 const images = importAll(require.context('../../Optos', false, /\.(png|jpe?g|svg)$/));
 const values = Object.values(images)
 values.sort();
-const tot=98;
+const tot=7;
 
 
 function sendData(user, loggedIn, selected, setSelected, imgSelected ,setImgSelected) {
