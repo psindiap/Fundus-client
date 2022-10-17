6e5d0345e13b8b4272f2cc0ef1ee9729079e0bfb
PSP <parthasarathipurkayastha001@gmail.com>
2022-10-17 14:35:21 +0530

    lolwa

--- src/Components/MagImg/MagImg.js
+++ src/Components/MagImg/MagImg.js
@@ -371,7 +371,7 @@ Magnification Mode:-
     }}
     class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Submit</button>
 </div>
-<div  className="flex space-x-2 justify-center">
+<div  className="flex space-x-2 justify-center pd">
 <a href="https://fundus-image.netlify.app/"><button 
     type="button"
     data-mdb-ripple="true"
